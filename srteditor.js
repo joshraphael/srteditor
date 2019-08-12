bbc4149513a0e8a21cb7709abeb3de335a46b6fa
Joshua Raphael <josh.n.raphael@gmail.com>
2019-08-12 14:02:42 -0700

    Refactored plugins, added source code plugin

--- srteditor.js
+++ srteditor.js
@@ -12,11 +12,12 @@ $(function() {
 
 function srteditor(area) {
     this.pluginIds = [
-        bold,
-        italic,
-        underline,
-        orderedList,
-        unorderedList
+        this.bold,
+        this.italic,
+        this.underline,
+        this.orderedList,
+        this.unorderedList,
+        this.source
     ];
     this.plugins = {};
     this.area = $(area);
@@ -24,6 +25,7 @@ function srteditor(area) {
     this.area.width("100%");
     this.area[0].contentDocument.designMode = "on";
     this.createToolbar();
+    this.createSourceBox();
 }
 
 srteditor.prototype.createToolbar = function() {
@@ -38,48 +40,96 @@ srteditor.prototype.createToolbar = function() {
     this.toolbar.insertBefore(this.area);
 };
 
+srteditor.prototype.createSourceBox = function() {
+    this.source = $("<code>");
+    this.source.insertAfter(this.area);
+    this.source.toggle();
+};
+
 srteditor.prototype.registerPlugin = function(p) {
     var self = this;
     var plugin = p();
     var btn = $("<span>");
-    var icon = $("<i>")
-    icon.addClass("fas")
-    icon.addClass(plugin.icon)
-    btn.append(icon)
-    btn.attr("id", plugin.cmd);
+    var icon = $("<i>");
+    icon.addClass("fas");
+    icon.addClass(plugin.icon);
+    btn.append(icon);
+    btn.attr("id", plugin.id);
     btn.height(24);
     btn.width(24);
-    btn.on("click", function() {
-        self.area[0].contentDocument.execCommand(plugin.cmd, false, plugin.args);
-    });
+    btn.on("click", {
+        src: self,
+        args: plugin.args
+    }, plugin.cmd);
     this.plugins[plugin.id] = plugin;
-    
     this.toolbar.append(btn);
 };
 
-function plugin(id, cmd, args, icon) {
+srteditor.prototype.bold = function() {
+    return new plugin("B", "fa-bold", style, {
+        cmd: "bold",
+        arg1: null
+    }, true);
+};
+
+srteditor.prototype.italic = function() {
+    return new plugin("I", "fa-italic", style, {
+        cmd: "italic",
+        arg1: null
+    }, true);
+};
+
+srteditor.prototype.underline = function() {
+    return new plugin("U", "fa-underline", style, {
+        cmd: "underline",
+        arg1: null
+    }, true);
+};
+
+srteditor.prototype.unorderedList = function() {
+    return new plugin("UL", "fa-list-ul", style, {
+        cmd: "InsertUnorderedList",
+        arg1: "newUL"
+    }, true);
+};
+
+srteditor.prototype.orderedList = function() {
+    return new plugin("OL", "fa-list-ol", style, {
+        cmd: "InsertOrderedList",
+        arg1: "newOL"
+    }, true);
+};
+
+srteditor.prototype.source = function() {
+    return new plugin("S", "fa-code", toggleSourceCode, null, false);
+};
+
+function plugin(id, icon, cmd, args, disable) {
     this.id = id;
+    this.icon = icon;
     this.cmd = cmd;
     this.args = args;
-    this.icon = icon;
+    this.disable = disable;
 }
 
-function bold() {
-    return new plugin("B", "bold", null, "fa-bold");
+function style(e) {
+    var self = e.data.src;
+    var args = e.data.args;
+    self.area[0].contentDocument.execCommand(args.cmd, false, args.arg1);
 }
 
-function italic() {
-    return new plugin("I", "italic", null, "fa-italic");
+function toggleSourceCode(e) {
+    var self = e.data.src;
+    self.area.toggle();
+    self.source.toggle();
+    self.source.text(self.area[0].contentDocument.body.innerHTML);
+    disablePlugins(self.plugins);
 }
 
-function underline() {
-    return new plugin("U", "underline", null, "fa-underline");
-}
-
-function unorderedList() {
-    return new plugin("UL", "InsertUnorderedList", "newUL", "fa-list-ul")
-}
-
-function orderedList() {
-    return new plugin("OL", "InsertOrderedList", "newOL", "fa-list-ol")
+function disablePlugins(plugins) {
+    for(var plugin in plugins) {
+        if(plugins[plugin].disable == true) {
+            $("#" + plugins[plugin].id).toggle();
+        }
+    }
 }
